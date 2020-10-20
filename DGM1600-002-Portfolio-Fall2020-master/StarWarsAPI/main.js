0821d8ec821b2feb1cdb19f1dfad74d5f2da3e66
Christopher Slinker <christopher.slinker@gmail.com>
2020-10-20 13:51:47 -0600

    Added .js persona nd film files and added a photo to Starwars API

--- DGM1600-002-Portfolio-Fall2020-master/StarWarsAPI/main.js
+++ DGM1600-002-Portfolio-Fall2020-master/StarWarsAPI/main.js
@@ -1,3 +1,20 @@
 import { films } from './data/films.js'
+import { people } from './data/people.js'
 
-console.log(films.length)
+console.log(people.length)
+
+const main = document.querySelector('')
+
+/*
+films.forEach(film => {
+    console.log(film.title)
+    let newTitle = main.appendChild(document.createElement('h1'))
+    newTitle.textContent = film.title
+})
+*/
+people.forEach(person => {
+console.log(people.name)
+let personImg = document.createElement('img')
+personImg.src = "https://i.imgur.com/P3kT4Te.jpg"
+main.appendChild(personImg)
+})
