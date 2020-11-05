aa0ed8ab5e9dd54922c879cc4e385432a978907e
Christopher Slinker <christopher.slinker@gmail.com>
2020-11-05 14:49:02 -0700

    more images, CSS API page, started to add JS

---
+++ DGM1600-002-Portfolio-Fall2020-master/Planets/main.js
@@ -0,0 +1,22 @@
+/*
+import { films } from './data/films.js'
+import { people } from './data/people.js'
+
+//console.log(people.length)
+
+const main = document.querySelector('main')
+
+for (let i = 0; i < 7; i++){
+    let figure = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = 'https://starwars-visualguide.com/assets/img/films/' + (i + 1) + '.jpg'
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = films[i].title
+
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
+
+    main.appendChild(figure)
+}
+*/
+
