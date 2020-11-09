f2b95595995349ca8b15941a8ec0c40c10a0ba27
Christopher Slinker <christopher.slinker@gmail.com>
2020-11-08 23:15:48 -0700

    Did a little bit of JS mostly styling and photo changing in HTML and CSS

---
+++ DGM1600-002-Portfolio-Fall2020-master/Characters/main.js
@@ -0,0 +1,74 @@
+import { people } from '../data/people.js'
+
+const mainContent = document.querySelector('#main')
+
+populateDOM(people)
+
+const mainHeader = document.createElement('header')
+mainHeader.className = 'mainHeader'
+document.body.insertBefore(mainHeader, mainContent)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male'
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female'
+mainHeader.appendChild(femaleButton)
+
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other'
+mainHeader.appendChild(otherButton)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+
+const femaleCharacters = people.filter(person => person.gender === 'female')
+
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'n/a' ||
+        person.gender === 'none' ||
+        person.gender === 'hermaphrodite') {
+        return person
+    }
+})
+
+maleButton.addEventListener('click', (event) => {
+   populateDOM(maleCharacters)
+})
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach(element => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true) // genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+    })
+}
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+function removeChildren(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
